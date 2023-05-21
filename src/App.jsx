d14314ceb5459716d66462df3faf6b4ad8a9685f
Bevan Lewis <blew0009@student.monash.edu>
2023-05-21 12:53:58 +1000

    Added background images

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
-import styles from "./styles/App.module.css"
-// import image01 from "./images/image01.jpg";
+import styles from "./styles/App.module.css";
+
+const backgroundImageStyles = [
+  styles.image001,
+    styles.image002,
+    styles.image003,
+    styles.image004,
+    styles.image005,
+    styles.image006,
+];
 
 function App() {
+  const [backgroundImage, setBackgroundImage] = useState("");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(
+      Math.random() * backgroundImageStyles.length
+    );
+    const randomImageStyle = backgroundImageStyles[randomIndex];
+    setBackgroundImage(randomImageStyle);
+    console.log("image");
+    console.log(backgroundImage);
+  }, [backgroundImage]);
+
   return (
     <>
-      <div className={styles.content}>
+      <div className={`${styles.content} ${backgroundImage}`}>
         <Header />
         <MainContent></MainContent>
       </div>
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import styles from "../styles/MainContent.module.css";
 
-import QUOTES from "../quotelist";
+import QUOTES from "../constants/quotelist";
 
 function MainContent() {
-  const [quote, setQuote] = useState(QUOTES[0]);
+  const [quote, setQuote] = useState("");
 
   const getRandomQuote = () => {
     const randomIndex = Math.floor(Math.random() * QUOTES.length);
@@ -18,8 +18,14 @@ function MainContent() {
   return (
     <div className={styles.mainContent}>
       <div className={styles.quoteHolder}>
-        <div className={styles.quote}>{`${quote.quote}`} </div>
-        <div className={styles.author}>{`-${quote.author}`}</div>
+        {quote ? (
+          <div>
+            <div className={styles.quote}>{`${quote.quote}`} </div>
+            <div className={styles.author}>{`-${quote.author}`}</div>
+          </div>
+        ) : (
+          <div>"Loading..."</div>
+        )}
       </div>
     </div>
   );
